1ec48755c2e7a449994548d86e5d4e4a9f7afc2b
flavio912 <flavio.arcaro912@gmail.com>
2020-03-19 11:43:48 +0000

    working in local docker

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/static', express.static(path.join(__dirname, '/build/static')));
 app.use('/images', express.static(path.join(__dirname, '/build/images')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
