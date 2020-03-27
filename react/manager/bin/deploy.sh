d8b544ab9ec87c2788d228ad43b5b380c85ed01b
flavio912 <flavio.arcaro912@gmail.com>
2020-03-27 23:00:22 +0000

    Use separate tags

--- react/manager/bin/deploy.sh
+++ react/manager/bin/deploy.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-TAG=$CI_PIPELINE_ID
+TAG=$CI_PIPELINE_ID-manager
 PREFIX="$EB_APPLICATION"
 
 zip app.zip ./* -r
 
-aws s3 cp app.zip s3://$EB_BUCKET/$PREFIX/app-$CI_PIPELINE_ID.zip
+aws s3 cp app.zip s3://$EB_BUCKET/$PREFIX/app-$TAG.zip
 rm app.zip
 
 echo "Creating new EB version"
-echo $CI_PIPELINE_ID
+echo $TAG
 aws elasticbeanstalk create-application-version \
     --application-name $EB_APPLICATION \
-      --version-label $CI_PIPELINE_ID \
-        --source-bundle S3Bucket=$EB_BUCKET,S3Key=$PREFIX/app-$CI_PIPELINE_ID.zip
+      --version-label $TAG \
+        --source-bundle S3Bucket=$EB_BUCKET,S3Key=$PREFIX/app-$TAG.zip
 
 echo "Updating EB environment"
 aws elasticbeanstalk update-environment \
     --environment-name $EB_ENV \
-      --version-label $CI_PIPELINE_ID
+      --version-label $TAG
