1730c9dd476775b09b79e4291fc227b63366dedb
Samuel Newman <mozzius@protonmail.com>
2020-04-07 16:28:11 +0100

    added little hyperlink

--- react/manager/src/components/LoginDialogue/LoginDialogue.tsx
+++ react/manager/src/components/LoginDialogue/LoginDialogue.tsx
@@ -22,6 +22,12 @@ const useStyles = createUseStyles((theme: Theme) => ({
     marginTop: 0,
     marginBottom: theme.spacing(2),
   },
+  link: {
+    margin: [15, 0, 0, 0],
+    textAlign: "center",
+    color: theme.colors.textBlue,
+    fontSize: theme.fontSizes.small,
+  },
 }));
 
 type Props = {};
@@ -38,6 +44,9 @@ function LoginDialogue(props: Props) {
       <FancyInput label="Email" labelColor={"#5CC301"} type={"email"} />
       <FancyInput label="Password" labelColor={"#5CC301"} type={"password"} />
       <FancyButton text="Login to TTC" />
+      <a className={classes.link} href="">
+        I don't have a TTC Hub account
+      </a>
     </Card>
   );
 }
