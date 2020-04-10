7d8d77d52a52b743677a7ec45b1c7a85f5c85837
flavio912 <flavio.arcaro912@gmail.com>
2020-04-10 13:28:46 +0100

    Add grant to the context

--- api/handler/auth.go
+++ api/handler/auth.go
@@ -4,15 +4,31 @@ import (
 	"context"
 	"net/http"
 	"strings"
+
+	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/middleware"
 )
 
+var (
+	// AuthKey is the contextKey for the JWT Auth token
+	AuthKey = contextKey("token")
+	// GrantKey is the contextKey for getting a grant
+	GrantKey = contextKey("grant")
+)
+
+// AuthHandler handles creating a grant for authenticated users
 func AuthHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		// todo: Pass JWT to resolvers for now; this should be moved to middleware
 		token := strings.ReplaceAll(r.Header.Get("Authorization"), "Bearer ", "")
 
-		ctx = context.WithValue(ctx, "token", token)
+		// Attempt to get a grant
+		grant, err := middleware.Authenticate(token)
+		if err == nil {
+			ctx = context.WithValue(ctx, GrantKey, grant)
+		}
+
+		ctx = context.WithValue(ctx, AuthKey, token)
 
 		h.ServeHTTP(w, r.WithContext(ctx))
 	})
