12804f8fca004a151cca173b20c75381235de9ec
Rory Anderson <rory@jragon.co.uk>
2020-04-17 13:38:32 +0100

    fix nil pointer exceptions on grant

--- api/resolvers/queries_test.go
+++ api/resolvers/queries_test.go
@@ -32,3 +32,61 @@ func TestAdmin(t *testing.T) {
 		},
 	})
 }
+
+func TestManager(t *testing.T) {
+	gqltesting.RunTests(t, []*gqltesting.Test{
+		{
+			Context: adminContext,
+			Schema:  schema,
+			Query: `
+				{
+					manager(uuid: "00000000-0000-0000-0000-000000000001") {
+						uuid
+						firstName
+						email
+					}
+				}
+			`,
+			ExpectedResult: `
+				{
+					"manager": {
+					  "uuid": "00000000-0000-0000-0000-000000000001",
+					  "email": "man@managers.com",
+					  "firstName": "Manager"
+					}
+				}
+			`,
+		},
+	})
+}
+
+func TestCompany(t *testing.T) {
+	gqltesting.RunTests(t, []*gqltesting.Test{
+		{
+			Context: adminContext,
+			Schema:  schema,
+			Query: `
+				{
+					company(uuid:"00000000-0000-0000-0000-000000000001") {
+							uuid
+							name
+							address {
+							  postCode
+							}
+					}
+				}
+			`,
+			ExpectedResult: `
+				{
+					"company": {
+					  "uuid": "00000000-0000-0000-0000-000000000001",
+						"name": "TestCompany",
+						"address": {
+							"postCode": "IP24RF"
+						}
+					}
+				}
+			`,
+		},
+	})
+}
