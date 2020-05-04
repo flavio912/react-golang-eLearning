6c2eb98619b6bdbc84a625286beb66d798c6513d
Archie Godfrey <archiegod@gmail.com>
2020-05-04 11:41:37 +0100

    Added optional colour border

--- react/delegate/src/sharedComponents/core/CircleBorder/CircleBorder.stories.tsx
+++ react/delegate/src/sharedComponents/core/CircleBorder/CircleBorder.stories.tsx
@@ -8,10 +8,11 @@ export default {
 };
 
 export const normal = () => {
+  const colour: string = text("Colour", "#FFFFFF");
   const name: string = text("Name", "Fred Ecceleston");
   const url: string = text("Profile Image", require("../../../assets/SampleImage_ClassroomCoursesDetail_Feat.png"));
   const user: User = { name, url };
   const size: number = number("Size", 45);
   const fontSize: number = number("fontSize", 18);
-  return <CircleBorder user={user} size={size} fontSize={fontSize} />;
+  return <CircleBorder user={user} size={size} fontSize={fontSize} colour={colour} />;
 };
