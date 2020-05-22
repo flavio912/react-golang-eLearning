af2f3372cf02e60bd42944bfbe8b84c4fbf5b507
flavio912 <flavio.arcaro912@gmail.com>
2020-05-22 10:53:39 +0100

    Add shadow prop

--- react/delegate/src/components/FlatCard/FlatCard.tsx
+++ react/delegate/src/components/FlatCard/FlatCard.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createUseStyles, useTheme } from 'react-jss';
 import classNames from 'classnames';
 import { Theme } from 'helpers/theme';
+import { boolean } from '@storybook/addon-knobs';
 
 const useStyles = createUseStyles((theme: Theme) => ({
   flatCardRoot: {
@@ -13,20 +14,29 @@ const useStyles = createUseStyles((theme: Theme) => ({
     display: 'flex',
     boxSizing: 'border-box',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    boxShadow: ({ shadow }: { shadow: boolean }) =>
+      shadow ? '2px 2px 10px 0 rgba(0,0,0,0.07)' : ''
   }
 }));
 
 type Props = {
   className?: string;
   children: React.ReactNode;
+  shadow: boolean;
   style?: any;
   backgroundColor?: string;
 };
 
-function FlatCard({ className, style, children, backgroundColor }: Props) {
+function FlatCard({
+  className,
+  style,
+  children,
+  backgroundColor,
+  shadow
+}: Props) {
   const theme = useTheme();
-  const classes = useStyles({ theme });
+  const classes = useStyles({ shadow, theme });
   style = {
     ...style,
     backgroundColor
--- react/delegate/src/components/FlatCard/FlatCart.stories.tsx
+++ react/delegate/src/components/FlatCard/FlatCart.stories.tsx
@@ -1,21 +1,20 @@
-import * as React from "react";
-import { withKnobs, text } from "@storybook/addon-knobs";
-import FlatCard from "./FlatCard";
+import * as React from 'react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import FlatCard from './FlatCard';
 
 export default {
-  title: "FlatCard",
-  decorators: [withKnobs],
+  title: 'FlatCard',
+  decorators: [withKnobs]
 };
 
 export const plain = () => {
   return (
     <FlatCard
-      className={text("className", "flat-card")}
+      className={text('className', 'flat-card')}
       children={<div>This is a test</div>}
-      backgroundColor={"red"}
-      style={{
-        boxShadow: `2px 2px 10px 0 rgba(0,0,0,0.07)`,
-      }}
+      backgroundColor={'white'}
+      shadow={boolean('Shadow', false)}
+      style={{}}
     />
   );
 };
