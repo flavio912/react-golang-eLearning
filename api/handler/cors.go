f5cc5c02c7919e1fcea8ecc7f21c7cb9a7eff00e
flavio912 <flavio.arcaro912@gmail.com>
2020-06-11 09:39:17 +0100

    Get CORS to allow all (for now)

--- api/handler/cors.go
+++ api/handler/cors.go
@@ -5,7 +5,8 @@ import "net/http"
 // TODO: Reevaluate CORS policy
 func CORSMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		// TODO: DONT USE THIS IN PROD
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
