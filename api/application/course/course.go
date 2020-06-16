e3d220ffe3529ab888e2aca2b20fa7113c39feae
flavio912 <flavio.arcaro912@gmail.com>
2020-06-16 16:13:27 +0100

    Clean up

--- api/application/course/course.go
+++ api/application/course/course.go
@@ -6,51 +6,8 @@ import (
 	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/application"
 	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/errors"
 	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/gentypes"
-	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/middleware"
-	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/middleware/course"
-	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/middleware/user"
 	"gitlab.codesigned.co.uk/ttc-heathrow/ttc-project/admin-react/api/models"
 )
-
-type CourseApp interface {
-	PurchaseCourses(input gentypes.PurchaseCoursesInput) (*gentypes.PurchaseCoursesResponse, error)
-
-	Course(courseID uint) (gentypes.Course, error)
-	Courses(courseIDs []uint) ([]gentypes.Course, error)
-	GetCourses(page *gentypes.Page, filter *gentypes.CourseFilter, orderBy *gentypes.OrderBy) ([]gentypes.Course, gentypes.PageInfo, error)
-
-	SaveOnlineCourse(courseInfo gentypes.SaveOnlineCourseInput) (gentypes.Course, error)
-	SaveClassroomCourse(courseInfo gentypes.SaveClassroomCourseInput) (gentypes.Course, error)
-
-	CreateTag(input gentypes.CreateTagInput) (gentypes.Tag, error)
-	GetTagsByCourseInfoIDs(ids []uint) (map[uint][]gentypes.Tag, error)
-	GetTags(page gentypes.Page, filter gentypes.GetTagsFilter, orderBy gentypes.OrderBy) ([]gentypes.Tag, error)
-	GetTagsByLessonUUID(uuid string) ([]gentypes.Tag, error)
-
-	GetLessonsByUUID(uuid []string) ([]gentypes.Lesson, error)
-	CreateLesson(lesson gentypes.CreateLessonInput) (gentypes.Lesson, error)
-	GetLessons(
-		page *gentypes.Page,
-		filter *gentypes.LessonFilter,
-		orderBy *gentypes.OrderBy,
-	) ([]gentypes.Lesson, gentypes.PageInfo, error)
-}
-
-type courseAppImpl struct {
-	grant             *middleware.Grant
-	ordersRepository  middleware.OrdersRepository
-	coursesRepository course.CoursesRepository
-	usersRepository   user.UsersRepository
-}
-
-func NewCourseApp(grant *middleware.Grant) CourseApp {
-	return &courseAppImpl{
-		grant:             grant,
-		ordersRepository:  middleware.NewOrdersRepository(&grant.Logger),
-		coursesRepository: course.NewCoursesRepository(&grant.Logger),
-		usersRepository:   user.NewUsersRepository(&grant.Logger),
-	}
-}
 
 // IsFullyApproved checks if a user is approved to view all restricted courses
 func (c *courseAppImpl) IsFullyApproved() bool {
--- api/application/users/manager.go
+++ api/application/users/manager.go
@@ -50,6 +50,90 @@ func (u *usersAppImpl) managersToGentype(managers []models.Manager) []gentypes.M
 	return genManagers
 }
 
+func (u *usersAppImpl) GetManagersByUUID(uuids []gentypes.UUID) ([]gentypes.Manager, error) {
+	// Manager can get own uuid, admin can get any
+	if !(len(uuids) == 1 && u.grant.Claims.UUID == uuids[0]) && !u.grant.IsAdmin {
+		return []gentypes.Manager{}, &errors.ErrUnauthorized
+	}
+
+	managers, err := u.usersRepository.GetManagersByUUID(uuids)
+	return u.managersToGentype(managers), err
+}
+
+func (u *usersAppImpl) GetManagerIDsByCompany(
+	companyUUID gentypes.UUID,
+	page *gentypes.Page,
+	filter *gentypes.ManagersFilter,
+	orderBy *gentypes.OrderBy,
+) ([]gentypes.UUID, gentypes.PageInfo, error) {
+	if !u.grant.IsAdmin {
+		return []gentypes.UUID{}, gentypes.PageInfo{}, &errors.ErrUnauthorized
+	}
+
+	return u.usersRepository.GetManagerIDsByCompany(companyUUID, page, filter, orderBy)
+}
+
+func (u *usersAppImpl) Delegate(uuid gentypes.UUID) (gentypes.Delegate, error) {
+	if !u.grant.IsAdmin && !u.grant.IsManager && !u.grant.IsDelegate {
+		return gentypes.Delegate{}, &errors.ErrUnauthorized
+	}
+
+	delegate, err := u.usersRepository.Delegate(uuid)
+
+	if !u.grant.IsAdmin &&
+		!(u.grant.IsManager && u.grant.Claims.Company == delegate.CompanyUUID) &&
+		!(u.grant.IsDelegate && u.grant.Claims.UUID == delegate.UUID) {
+		return gentypes.Delegate{}, &errors.ErrUnauthorized
+	}
+
+	return u.delegateToGentype(delegate), err
+}
+
+func (u *usersAppImpl) CreateManager(managerDetails gentypes.CreateManagerInput) (gentypes.Manager, error) {
+	if !u.grant.IsAdmin && !u.grant.IsManager {
+		return gentypes.Manager{}, &errors.ErrUnauthorized
+	}
+
+	var compUUID gentypes.UUID
+	// If you're an admin you need to provide the company UUID
+	if u.grant.IsAdmin {
+		if managerDetails.CompanyUUID != nil {
+			compUUID = *managerDetails.CompanyUUID
+		} else {
+			return gentypes.Manager{}, &errors.ErrCompanyNotFound
+		}
+	}
+
+	// If you're a manager the company UUID will be selected from the one in your JWT claims
+	if u.grant.IsManager {
+		compUUID = u.grant.Claims.Company
+	}
+
+	manager, err := u.usersRepository.CreateManager(managerDetails, compUUID)
+	return u.managerToGentype(manager), err
+}
+
+func (u *usersAppImpl) UpdateManager(input gentypes.UpdateManagerInput) (gentypes.Manager, error) {
+	if !(u.grant.IsManager && u.grant.Claims.UUID == input.UUID) && !u.grant.IsAdmin {
+		return gentypes.Manager{}, &errors.ErrUnauthorized
+	}
+
+	manager, err := u.usersRepository.UpdateManager(input)
+	return u.managerToGentype(manager), err
+}
+
+func (u *usersAppImpl) DeleteManager(uuid gentypes.UUID) (bool, error) {
+	// managers can delete themselves
+	// admins can delete any manager
+	if !(u.grant.IsManager && u.grant.Claims.UUID == uuid) && !u.grant.IsAdmin {
+		return false, &errors.ErrUnauthorized
+	}
+
+	// TODO: delete profile image from S3
+
+	return u.usersRepository.DeleteManager(uuid)
+}
+
 // ProfileUploadRequest generates a link that lets users upload a profile image to S3 directly
 // Used by all user types
 func (u *usersAppImpl) ProfileUploadRequest(imageMeta gentypes.UploadFileMeta) (string, string, error) {
