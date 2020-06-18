9aae86fed2d2887852c625999bf21f4ad4f834b1
flavio912 <flavio.arcaro912@gmail.com>
2020-06-18 13:43:52 +0100

    Small style changes
    - Change text to primaryBlack (not black)
    - Adjust paddings + border radius of card

--- react/publicSite/src/components/CourseSearch/CourseItem.tsx
+++ react/publicSite/src/components/CourseSearch/CourseItem.tsx
@@ -1,140 +1,145 @@
-import * as React from "react";
-import { createUseStyles, useTheme } from "react-jss";
+import * as React from 'react';
+import { createUseStyles, useTheme } from 'react-jss';
 import classNames from 'classnames';
-import { Theme } from "helpers/theme";
-import Button from "sharedComponents/core/Input/Button";
+import { Theme } from 'helpers/theme';
+import Button from 'sharedComponents/core/Input/Button';
 
 const useStyles = createUseStyles((theme: Theme) => ({
-    root: {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: theme.colors.primaryWhite,
-        boxShadow: '4px 2px 10px -2px rgba(0,0,0,0.06)',
-        border: ['1px', 'solid', theme.colors.borderGrey],
-        borderRadius: '13px',
-        padding: '24px 22px 24px 42px',
-    },
-    title: {
-        fontSize: theme.fontSizes.heading,
-        fontWeight: '800',
-        marginBottom: '20px'
-    },
-    description: {
-        fontSize: theme.fontSizes.default,
-        fontWeight: '500',
-        color: theme.colors.textGrey,
-        maxWidth: '600px',
-        margin: '0 50px 20px 0'
-    },
-    price: {
-        fontSize: theme.fontSizes.heading,
-        fontWeight: '800',
-        color: theme.colors.primaryBlue,
-        marginBottom: '20px'
-    },
-    button: {
-        height: '52px',
-        width: '182px',
-        boxShadow: '0 1px 4px 0 rgba(0,0,0,0.09)',
-        margin: '0 20px 20px 0',
-        fontSize: theme.fontSizes.large,
-        fontWeight: '800'
-    },
-    extraShadow: {
-        boxShadow: '0 1px 4px 0 rgba(0,0,0,0.43)',
-    },
-    image: {
-        display: 'flex',
-        height: '240px',
-        width: '287px',
-        borderRadius: `0 ${theme.primaryBorderRadius}px 0 0`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover'
-    },
-    type: {
-        width: '160px',
-        alignSelf: 'flex-start',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: theme.fontSizes.xSmall,
-        fontWeight: '700',
-        color: theme.colors.primaryWhite,
-        borderRadius: `0 0 ${theme.secondaryBorderRadius}px 0`,
-        padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`
-    },
-    column: {
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start'
-    },
-    row: {
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: theme.colors.primaryWhite,
+    boxShadow: '4px 2px 10px -2px rgba(0,0,0,0.06)',
+    border: ['1px', 'solid', theme.colors.borderGrey],
+    borderRadius: '13px',
+    padding: '24px 22px 24px 42px'
+  },
+  title: {
+    fontSize: theme.fontSizes.heading,
+    fontWeight: '800',
+    marginBottom: '20px',
+    color: theme.colors.primaryBlack
+  },
+  description: {
+    fontSize: theme.fontSizes.default,
+    fontWeight: '500',
+    color: theme.colors.textGrey,
+    maxWidth: '600px',
+    margin: '0 50px 20px 0'
+  },
+  price: {
+    fontSize: theme.fontSizes.heading,
+    fontWeight: '800',
+    color: theme.colors.primaryBlue,
+    marginBottom: '20px'
+  },
+  button: {
+    height: '52px',
+    width: '182px',
+    boxShadow: '0 1px 4px 0 rgba(0,0,0,0.09)',
+    margin: '0 20px 20px 0',
+    fontSize: theme.fontSizes.large,
+    fontWeight: '800'
+  },
+  extraShadow: {
+    boxShadow: '0 1px 4px 0 rgba(0,0,0,0.43)'
+  },
+  image: {
+    display: 'flex',
+    height: '240px',
+    width: '287px',
+    borderRadius: theme.secondaryBorderRadius,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  },
+  type: {
+    width: '160px',
+    alignSelf: 'flex-start',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: theme.fontSizes.xSmall,
+    fontWeight: '700',
+    color: theme.colors.primaryWhite,
+    borderRadius: `${theme.secondaryBorderRadius}px 0 ${theme.secondaryBorderRadius}px 0`,
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`
+  },
+  column: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 }));
 
 export type CourseProps = {
-    title: string;
-    description: string;
-    price: string;
-    type: string;
-    colour: string;
-    imageURL: string;
-    viewCourse: () => void;
-    addToBasket: () => void;
-    className?: string;
+  title: string;
+  description: string;
+  price: string;
+  type: string;
+  colour: string;
+  imageURL: string;
+  viewCourse: () => void;
+  addToBasket: () => void;
+  className?: string;
 };
 
-function CourseItem({ title, description, price, type, colour, imageURL, viewCourse, addToBasket, className }: CourseProps) {
-    const theme = useTheme();
-    const classes = useStyles({ theme });
+function CourseItem({
+  title,
+  description,
+  price,
+  type,
+  colour,
+  imageURL,
+  viewCourse,
+  addToBasket,
+  className
+}: CourseProps) {
+  const theme = useTheme();
+  const classes = useStyles({ theme });
 
-    const backgroundColor = { backgroundColor: colour };
-    const backgroundImage = {};
-    if (imageURL)
-        backgroundImage[
-        'backgroundImage'
-        ] = `linear-gradient(#00000040, #00000040), url(${imageURL})`;
+  const backgroundColor = { backgroundColor: colour };
+  const backgroundImage = {};
+  if (imageURL)
+    backgroundImage[
+      'backgroundImage'
+    ] = `linear-gradient(#00000040, #00000040), url(${imageURL})`;
 
   return (
     <div className={classNames(classes.root, className)}>
-        <div className={classes.column}>
-            <div className={classes.title}>{title}</div>
-            <div className={classes.description}>{description}</div>
-            <div className={classes.price}>{price}</div>
-            <div className={classes.row}>
-                <Button
-                    archetype="submit"
-                    onClick={viewCourse}
-                    className={classNames(classes.button, classes.extraShadow)}
-                >
-                    View Course
-                </Button>
-                <Button
-                    onClick={addToBasket}
-                    className={classes.button}
-                >
-                    Add to Basket
-                </Button>
-            </div>
+      <div className={classes.column}>
+        <div className={classes.title}>{title}</div>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{price}</div>
+        <div className={classes.row}>
+          <Button
+            archetype="submit"
+            onClick={viewCourse}
+            className={classNames(classes.button, classes.extraShadow)}
+          >
+            View Course
+          </Button>
+          <Button onClick={addToBasket} className={classes.button}>
+            Add to Basket
+          </Button>
         </div>
-        <div
-            className={classes.image}
-            style={backgroundImage}
-        >
-            <div className={classes.type} style={backgroundColor}>
-                {type.toUpperCase()}
-            </div>
+      </div>
+      <div className={classes.image} style={backgroundImage}>
+        <div className={classes.type} style={backgroundColor}>
+          {type.toUpperCase()}
         </div>
+      </div>
     </div>
   );
 }
