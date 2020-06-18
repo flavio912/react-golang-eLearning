c9187a2b48efcada48667f065b638c8163423a45
Archie Godfrey <archiegod@gmail.com>
2020-06-18 14:05:32 +0100

    Added article components

---
+++ react/publicSite/src/components/Overview/Article/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { createUseStyles, useTheme } from "react-jss";
+import classNames from "classnames";
+import { Theme } from "helpers/theme";
+
+const useStyles = createUseStyles((theme: Theme) => ({
+    root: {
+        height: '370px',
+        width: '402px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        boxShadow: '4px 9px 39px -22px rgba(0,0,0,0.18)',
+        borderRadius: '3px'
+    },
+    image: {
+        height: '241px',
+        width: '402px',
+        borderRadius: '3px 3px 0 0'
+    },
+    type: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '160px',
+        height: '33px',
+        backgroundColor: theme.colors.navyBlue,
+        fontSize: theme.fontSizes.xSmall,
+        color: theme.colors.primaryWhite,
+        fontWeight: '800',
+        borderRadius: '3px 0 10px 0'
+    },
+    date: {
+        fontSize: theme.fontSizes.xSmall,
+        fontWeight: '800',
+        color: theme.colors.textGrey,
+        margin: '25px 0 20px 0'
+    },
+    description: {
+        textAlign: 'center',
+        fontSize: theme.fontSizes.extraLarge,
+        margin: '0 25px 20px 25px'
+    }
+}));
+
+type Props = {
+    type: string;
+    imageURL: string;
+    date: string;
+    description: string;
+    className?: string;
+};
+
+function ArticleCard({ type, imageURL, date, description, className }: Props) {
+    const theme = useTheme();
+    const classes = useStyles({ theme });
+
+  return (
+      <div className={classNames(classes.root, className)}>
+          <div className={classes.image} style={{ backgroundImage: `url(${imageURL})` }}>
+              <div className={classes.type}>{type.toUpperCase()}</div>
+          </div>
+          <div className={classes.date}>{date}</div>
+          <div className={classes.description}>{description}</div>
+      </div>
+  );
+}
+
+export default ArticleCard;
---
+++ react/publicSite/src/components/Overview/Article/ArticleHeader/ArticleHeader.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import { createUseStyles, useTheme } from "react-jss";
+import classNames from "classnames";
+import { Theme } from "helpers/theme";
+import CircleBorder, { User } from "sharedComponents/core/CircleBorder";
+
+const useStyles = createUseStyles((theme: Theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        padding: '140px 200px 75px 200px'
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 46,
+        fontWeight: '800',
+        color: theme.colors.primaryWhite,
+        maxWidth: '615px',
+        margin: '20px 0'
+    },
+    detail: {
+        fontSize: theme.fontSizes.large,
+        color: theme.colors.primaryWhite
+    },
+    name: {
+        fontSize: theme.fontSizes.extraLarge,
+        fontWeight: '800',
+        color: theme.colors.primaryWhite,
+        marginBottom: '5px'
+    },
+    profile: {
+        marginRight: '20px'
+    },
+    italic: {
+        fontStyle: 'italic',
+        fontSize: theme.fontSizes.default,
+    },
+    underline: {
+        textDecorationLine: 'underline'
+    }
+}));
+
+type Props = {
+    title: string;
+    date: string;
+    image: string;
+    featured?: string;
+    genre?: string;
+    author?: User;
+    className?: string;
+};
+
+function ArticleHeader({ title, date, image, featured, genre, author, className }: Props) {
+    const theme = useTheme();
+    const classes = useStyles({ theme });
+
+  return (
+      <div
+        className={classNames(classes.root, className)}
+        style={{ backgroundImage: `url(${image})` }}
+    >
+          {featured && (
+            <div className={classes.detail}>{featured}  •  {date}</div>
+          )}
+
+          <div className={classNames(classes.title, featured && classes.underline)}>{title}</div>
+
+          {author && (
+            <div className={classes.row}>
+                <CircleBorder user={author} size={50} className={classes.profile}/>
+                <div>
+                    <div className={classes.name}>{author.name}</div>
+                    <div className={classNames(classes.detail, classes.italic)}>{date} • {genre}</div>
+                </div>
+            </div>
+          )}
+      </div>
+  );
+}
+
+export default ArticleHeader;
