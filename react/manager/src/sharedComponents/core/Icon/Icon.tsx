b853704aa3ef83a7ab9cebe185d5d37191bedc78
Hoang Hung <hoang@codesigned.co.uk>
2020-06-21 10:28:11 +0700

    fix: Mp3 component

--- react/manager/src/sharedComponents/core/Icon/Icon.tsx
+++ react/manager/src/sharedComponents/core/Icon/Icon.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 
 export type IconNames =
   | 'AddDelegateRepeater'
@@ -43,56 +43,58 @@ export type IconNames =
   | 'TTC_Logo_Icon'
   | 'Mp3_Pause'
   | 'Mp3_Speaker'
+  | 'Mp3_Play'
   | 'Payments_Method'
   | 'Stripe'
   | 'Volume';
 
 const iconNameMap = {
-  AddDelegateRepeater: require("../../../assets/AddDelegateRepeater.svg"),
-  ArrowLeft: require("../../../assets/ArrowLeft.svg"),
-  ArrowLeftBlue: require("../../../assets/ArrowLeftBlue.svg"),
-  ArrowLeftNavyBlue: require("../../../assets/ArrowLeftNavyBlue.svg"),
-  ArrowRight: require("../../../assets/ArrowRight.svg"),
-  ArrowRightNavyBlue: require("../../../assets/ArrowRightNavyBlue.svg"),
-  Card_SecondaryActon_Dots: require("../../../assets/Card_SecondaryActon_Dots.svg"),
-  CloseCourseManagementTray_X: require("../../../assets/CloseCourseManagementTray_X.svg"),
-  CourseAccountActivated: require("../../../assets/CourseAccountActivated.svg"),
-  CourseCertificates: require("../../../assets/CourseCertificates.svg"),
-  CourseExpiringSoon: require("../../../assets/CourseExpiringSoon.svg"),
-  CourseFailed: require("../../../assets/CourseFailed.svg"),
-  CourseNewCourse: require("../../../assets/CourseNewCourse.svg"),
-  CourseNewCourseGreen: require("../../../assets/CourseNewCourseGreen.svg"),
-  CourseNewCourseGrey: require("../../../assets/CourseNewCourseGrey.svg"),
-  CourseStatus_Completed: require("../../../assets/CourseStatus_Completed.svg"),
-  CourseStatus_Incomplete: require("../../../assets/CourseStatus_Incomplete.svg"),
-  CourseStatus_NotStarted: require("../../../assets/CourseStatus_NotStarted.svg"),
-  CourseTimeTracked: require("../../../assets/CourseTimeTracked.svg"),
-  CourseTimeTrackedGreen: require("../../../assets/CourseTimeTrackedGreen.svg"),
-  CourseTimeTrackedGrey: require("../../../assets/CourseTimeTrackedGrey.svg"),
-  Course_Calendar: require("../../../assets/Course_Calendar.svg"),
-  DownloadCSV: require("../../../assets/DownloadCSV.svg"),
-  EditDelegateProfilePic_Default: require("../../../assets/EditDelegateProfilePic_Default.png"),
-  EditPicPencil: require("../../../assets/EditPicPencil.svg"),
-  FilterAdjust: require("../../../assets/FilterAdjust.svg"),
-  FormCheckbox_Checked: require("../../../assets/FormCheckbox_Checked.svg"),
-  FormCheckbox_states: require("../../../assets/FormCheckbox_states.png"),
-  FormCheckbox_Unchecked: require("../../../assets/FormCheckbox_Unchecked.svg"),
-  Icon_Delegates: require("../../../assets/Icon_Delegates.svg"),
-  LeftNav_Icon_Courses: require("../../../assets/LeftNav_Icon_Courses.svg"),
-  LeftNav_Icon_Dashboard: require("../../../assets/LeftNav_Icon_Dashboard.svg"),
-  LeftNav_Icon_Delegates: require("../../../assets/LeftNav_Icon_Delegates.svg"),
-  Loading_Screen_Donut: require("../../../assets/Loading_Screen_Donut.svg"),
-  Location_Pin: require("../../../assets/Location_Pin.svg"),
-  PDF_Icon: require("../../../assets/PDF_Icon.svg"),
-  RemoveSelectedCourse_X: require("../../../assets/RemoveSelectedCourse_X.svg"),
-  SampleImage_ClassroomCoursesDetail_Feat: require("../../../assets/SampleImage_ClassroomCoursesDetail_Feat.png"),
-  SearchGlass: require("../../../assets/SearchGlass.svg"),
-  TTC_Logo_Icon: require("../../../assets/logo/ttc-logo-icon.svg"),
+  AddDelegateRepeater: require('../../../assets/AddDelegateRepeater.svg'),
+  ArrowLeft: require('../../../assets/ArrowLeft.svg'),
+  ArrowLeftBlue: require('../../../assets/ArrowLeftBlue.svg'),
+  ArrowLeftNavyBlue: require('../../../assets/ArrowLeftNavyBlue.svg'),
+  ArrowRight: require('../../../assets/ArrowRight.svg'),
+  ArrowRightNavyBlue: require('../../../assets/ArrowRightNavyBlue.svg'),
+  Card_SecondaryActon_Dots: require('../../../assets/Card_SecondaryActon_Dots.svg'),
+  CloseCourseManagementTray_X: require('../../../assets/CloseCourseManagementTray_X.svg'),
+  CourseAccountActivated: require('../../../assets/CourseAccountActivated.svg'),
+  CourseCertificates: require('../../../assets/CourseCertificates.svg'),
+  CourseExpiringSoon: require('../../../assets/CourseExpiringSoon.svg'),
+  CourseFailed: require('../../../assets/CourseFailed.svg'),
+  CourseNewCourse: require('../../../assets/CourseNewCourse.svg'),
+  CourseNewCourseGreen: require('../../../assets/CourseNewCourseGreen.svg'),
+  CourseNewCourseGrey: require('../../../assets/CourseNewCourseGrey.svg'),
+  CourseStatus_Completed: require('../../../assets/CourseStatus_Completed.svg'),
+  CourseStatus_Incomplete: require('../../../assets/CourseStatus_Incomplete.svg'),
+  CourseStatus_NotStarted: require('../../../assets/CourseStatus_NotStarted.svg'),
+  CourseTimeTracked: require('../../../assets/CourseTimeTracked.svg'),
+  CourseTimeTrackedGreen: require('../../../assets/CourseTimeTrackedGreen.svg'),
+  CourseTimeTrackedGrey: require('../../../assets/CourseTimeTrackedGrey.svg'),
+  Course_Calendar: require('../../../assets/Course_Calendar.svg'),
+  DownloadCSV: require('../../../assets/DownloadCSV.svg'),
+  EditDelegateProfilePic_Default: require('../../../assets/EditDelegateProfilePic_Default.png'),
+  EditPicPencil: require('../../../assets/EditPicPencil.svg'),
+  FilterAdjust: require('../../../assets/FilterAdjust.svg'),
+  FormCheckbox_Checked: require('../../../assets/FormCheckbox_Checked.svg'),
+  FormCheckbox_states: require('../../../assets/FormCheckbox_states.png'),
+  FormCheckbox_Unchecked: require('../../../assets/FormCheckbox_Unchecked.svg'),
+  Icon_Delegates: require('../../../assets/Icon_Delegates.svg'),
+  LeftNav_Icon_Courses: require('../../../assets/LeftNav_Icon_Courses.svg'),
+  LeftNav_Icon_Dashboard: require('../../../assets/LeftNav_Icon_Dashboard.svg'),
+  LeftNav_Icon_Delegates: require('../../../assets/LeftNav_Icon_Delegates.svg'),
+  Loading_Screen_Donut: require('../../../assets/Loading_Screen_Donut.svg'),
+  Location_Pin: require('../../../assets/Location_Pin.svg'),
+  PDF_Icon: require('../../../assets/PDF_Icon.svg'),
+  RemoveSelectedCourse_X: require('../../../assets/RemoveSelectedCourse_X.svg'),
+  SampleImage_ClassroomCoursesDetail_Feat: require('../../../assets/SampleImage_ClassroomCoursesDetail_Feat.png'),
+  SearchGlass: require('../../../assets/SearchGlass.svg'),
+  TTC_Logo_Icon: require('../../../assets/logo/ttc-logo-icon.svg'),
   Mp3_Pause: require('../../../assets/Mp3_Pause.svg'),
   Mp3_Speaker: require('../../../assets/Mp3_Speaker.svg'),
-  Payments_Method: require("../../../assets/Payments_Method.svg"),
-  Stripe: require("../../../assets/Stripe.svg"),
-  Volume: require("../../../assets/Volume.svg")
+  Mp3_Play: require('../../../assets/Mp3_Play.svg'),
+  Payments_Method: require('../../../assets/Payments_Method.svg'),
+  Stripe: require('../../../assets/Stripe.svg'),
+  Volume: require('../../../assets/Volume.svg')
 };
 
 type Props = {
@@ -109,14 +111,14 @@ function Icon({
   pointer,
   ...props
 }: Props & React.ImgHTMLAttributes<HTMLImageElement>) {
-  const cursor = pointer ? "pointer" : "auto";
+  const cursor = pointer ? 'pointer' : 'auto';
   const sizeStyle = size ? { height: size, width: size } : {};
   return (
     <img
       style={{
         cursor,
         ...sizeStyle,
-        ...style,
+        ...style
       }}
       src={iconNameMap[name]}
       alt={`${name} icon`}
