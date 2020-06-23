8e0a9990443d12b1015ef848f2039837532fca53
flavio912 <flavio.arcaro912@gmail.com>
2020-06-23 13:37:48 +0100

    Start adding submit test api

--- api/models/course_taker.go
+++ api/models/course_taker.go
@@ -14,19 +14,28 @@ type CourseTaker struct {
 
 // ActiveCourse represents a course the taker is assigned and can complete
 type ActiveCourse struct {
+	UUID            gentypes.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	CourseTaker     CourseTaker
 	CourseTakerUUID gentypes.UUID `gorm:"primary_key"`
 	Course          Course
 	CourseID        uint `gorm:"primary_key;auto_increment:false"` // FKEY
-	CurrentAttempt  uint `gorm:"default:1"`
+	CurrentAttempt  uint `gorm:"default:1"`                        // Starts at 1
 	MinutesTracked  float64
 }
 
 type CourseTakerActivity struct {
 	UUID            gentypes.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	CreatedAt       time.Time
-	CourseTakerUUID gentypes.UUID
+	CourseTakerUUID gentypes.UUID         `gorm:"type:uuid;"`
 	ActivityType    gentypes.ActivityType // The type of activity
 	CourseID        *uint
 	Course          Course
+}
+
+type TestMark struct {
+	TestUUID   gentypes.UUID `gorm:"type:uuid;"` //FKEY
+	NumCorrect uint
+	// Total keeps its own record of total marks in case the
+	// test changes in between a user completing this test and completing the course
+	Total uint
 }
