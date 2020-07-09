dc9153682fe05c9311a3c45a24637d2a05d2c103
Hesham Haikal <heshamrhaikal@gmail.com>
2020-07-09 22:35:57 +0100

    Add mutation test for deleteModule

--- api/resolvers/mutations_test.go
+++ api/resolvers/mutations_test.go
@@ -1369,3 +1369,42 @@ func TestCreateTest(t *testing.T) {
 		},
 	})
 }
+
+func TestDeleteModule(t *testing.T) {
+	prepareTestDatabase()
+
+	gqltest.RunTests(t, []*gqltest.Test{
+		{
+			Name:    "Delete module",
+			Context: adminContext(),
+			Schema:  schema,
+			Query: `
+				mutation {
+					deleteModule(input: {
+						uuid: "00000000-0000-0000-0000-000000000001"
+					})
+				}
+			`,
+			ExpectedResult: `
+				{
+					"deleteModule": true
+				}
+			`,
+		},
+	})
+
+	accessTest(t, schema, accessTestOpts{
+		Query: `
+			mutation {
+				deleteModule(input: {
+					uuid: "00000000-0000-0000-0000-000000000001"
+				})
+			}
+		`,
+		Path:            []interface{}{"deleteModule"},
+		MustAuth:        true,
+		AdminAllowed:    true,
+		ManagerAllowed:  false,
+		DelegateAllowed: false,
+	})
+}
