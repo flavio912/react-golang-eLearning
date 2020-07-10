9337db5ac0649c120f11610bfdc2c137d20bdbd4
Archie <archiegod@gmail.com>
2020-07-10 14:31:01 +0100

    Started on frontend

--- react/admin/src/views/Module/AudioVideo/AudioVideo.js
+++ react/admin/src/views/Module/AudioVideo/AudioVideo.js
@@ -5,17 +5,29 @@ import {
   CardHeader,
   CardContent,
   Divider,
+  TextField,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { Autocomplete } from '@material-ui/lab';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
+  thinInput: {
+    width: '30%'
+  },
+  shortDescription: {
+    width: '100%'
+  }
 }));
 
 function AudioVideo({}) {
   const classes = useStyles();
+  const onChange=()=>{};
+  const [name, setName] = React.useState();
+  const [description, setDescription] = React.useState();
+  const categoryOptions = [{ title: 'Aviation Security', value: 'avsec' }];
 
   return (
     <div className={classes.root}>
@@ -23,10 +35,66 @@ function AudioVideo({}) {
         <Grid container spacing={4} direction={'column'}>
           <Grid item>
             <Card>
-              <CardHeader title="Audio Voiceover" />
-              <Divider />
               <CardContent>
-                <div>TBA</div>
+                <Grid container spacing={2} direction={'column'}>
+                  <Grid item>
+                    <CardHeader title="Audio Voiceover" />
+                  </Grid>
+                  <Grid item>
+                    <Autocomplete
+                      options={categoryOptions}
+                      getOptionLabel={option => option.title}
+                      onChange={(event, newValue) => {
+                          onChange(newValue.value);
+                      }}
+                      renderInput={params => (
+                          <TextField
+                              {...params}
+                              label=""
+                              variant="outlined"
+                              className={classes.thinInput}
+                          />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Divider />
+                    <CardHeader title="Video Source" />
+                  </Grid>
+                  <Grid item>
+                    <Autocomplete
+                      options={categoryOptions}
+                      getOptionLabel={option => option.title}
+                      onChange={(event, newValue) => {
+                          onChange(newValue.value);
+                      }}
+                      renderInput={params => (
+                          <TextField
+                              {...params}
+                              label=""
+                              variant="outlined"
+                              className={classes.thinInput}
+                          />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <div>Select</div>
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      fullWidth
+                      label=""
+                      name="modulename"
+                      onChange={inp => {
+                          onChange(inp.target.value);
+                      }}
+                      placeholder="Enter Wisita Video URL"
+                      value={name}
+                      variant="outlined"
+                    />
+                  </Grid>
+                </Grid>
               </CardContent>
             </Card>
           </Grid>
@@ -35,7 +103,18 @@ function AudioVideo({}) {
               <CardHeader title="Video Transcript" />
               <Divider />
               <CardContent>
-                <div>TBA</div>
+                <TextField
+                  label=""
+                  multiline
+                  className={classes.shortDescription}
+                  rows={5}
+                  value={description}
+                  onChange={inp => {
+                      setDescription(inp.target.value);
+                  }}
+                  placeholder="Description"
+                  variant="outlined"
+                />
               </CardContent>
             </Card>
           </Grid>
--- react/admin/src/views/Module/Overview/Overview.js
+++ react/admin/src/views/Module/Overview/Overview.js
@@ -5,40 +5,99 @@ import {
   CardHeader,
   CardContent,
   Divider,
+  TextField,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import TagsInput from 'src/components/TagsInput';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
+  shortDescription: {
+    width: '100%'
+  }
 }));
 
 function Overview({}) {
   const classes = useStyles();
 
+  const onChange=()=>{};
+  const [name, setName] = React.useState();
+  const [description, setDescription] = React.useState();
+
   return (
     <div className={classes.root}>
       <Grid container spacing={2}>
-        <Grid container spacing={4} direction={'column'}>
-          <Grid item>
-            <Card>
-              <CardHeader title="About this Module" />
-              <Divider />
-              <CardContent>
-                <div>TBA</div>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card>
-              <CardHeader title="Module Description" />
-              <Divider />
-              <CardContent>
-                <div>TBA</div>
-              </CardContent>
-            </Card>
-          </Grid>
+        <Grid item={6} xs={8}>
+            <Grid container spacing={2} direction={'column'}>
+            <Grid item>
+                <Card>
+                <CardHeader title="About this Module" />
+                <Divider />
+                <CardContent>
+                    <Grid container spacing={4} direction={'column'}>
+                    <Grid item>
+                        <TextField
+                        fullWidth
+                        label="Module Name"
+                        name="modulename"
+                        onChange={inp => {
+                            onChange(inp.target.value);
+                        }}
+                        placeholder="e.g. Fire Safety Module 1"
+                        value={name}
+                        variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item>
+                    <TagsInput />
+                    </Grid>
+                    </Grid>
+                </CardContent>
+                </Card>
+            </Grid>
+            <Grid item>
+                <Card>
+                <CardHeader title="Module Description" />
+                <Divider />
+                <CardContent>
+                    <TextField
+                    label=""
+                    multiline
+                    className={classes.shortDescription}
+                    rows={5}
+                    value={description}
+                    onChange={inp => {
+                        setDescription(inp.target.value);
+                    }}
+                    placeholder="Description"
+                    variant="outlined"
+                    />
+                </CardContent>
+                </Card>
+            </Grid>
+            </Grid>
+        </Grid>
+        <Grid item xs={4}>
+        <Card>
+                <CardHeader title="Module Banner Image" />
+                <Divider />
+                <CardContent>
+                    <TextField
+                    label=""
+                    multiline
+                    className={classes.shortDescription}
+                    rows={5}
+                    value={description}
+                    onChange={inp => {
+                        setDescription(inp.target.value);
+                    }}
+                    placeholder="Description"
+                    variant="outlined"
+                    />
+                </CardContent>
+                </Card>
         </Grid>
       </Grid>
     </div>
