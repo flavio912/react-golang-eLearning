0f0e265da599f6ef6b56c86f33048c8bd328a198
Archie <archiegod@gmail.com>
2020-07-15 10:59:13 +0100

    Removed text field

--- react/admin/src/components/ReorderableList/ReorderableDropdown.js
+++ react/admin/src/components/ReorderableList/ReorderableDropdown.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Card, CardContent } from '@material-ui/core';
+import { Typography, Card, CardContent } from '@material-ui/core';
 import {TreeView, TreeItem} from '@material-ui/lab';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -9,11 +9,21 @@ import ReoderableList from './ReorderableList';
 
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
+  noSelect: {
+    '& .MuiTreeItem-content': {
+      padding: '10px 0',
+    },
+    '& .Mui-selected': {
+      // Currently not working, need to change this style:
+      // .MuiTreeItem-root.Mui-selected > .MuiTreeItem-content .MuiTreeItem-label
+      backgroundColor: 'transparent'
+    }
+  }
 });
 
-export default function ReoderableDropdown({ value, onChange, placeholder, label, items, setItems }) {
+export default function ReoderableDropdown({ title, items, setItems }) {
   const classes = useStyles();
   const [ expanded, setExpanded ] = React.useState(false);
 
@@ -28,14 +38,14 @@ export default function ReoderableDropdown({ value, onChange, placeholder, label
         >
           <TreeItem
             nodeId="1"
+            className={classes.noSelect}
             label={
-              <TextField
-                fullWidth
-                label={label}
-                onChange={onChange}
-                placeholder={placeholder}
-                value={value}
-              />
+              <Typography
+                variant="h5"
+                color="textPrimary"
+              >
+                {title}
+              </Typography>
             }
             endIcon={
                 expanded
