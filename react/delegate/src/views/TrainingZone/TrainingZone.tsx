8290b49adb55db287ba0bccd2316aa696d470a70
Archie <archiegod@gmail.com>
2020-08-03 14:18:05 +0100

    Added courses to jump back in

--- react/delegate/src/views/TrainingZone/TrainingZone.tsx
+++ react/delegate/src/views/TrainingZone/TrainingZone.tsx
@@ -11,6 +11,7 @@ import {
   TrainingZone_user,
   CourseStatus
 } from './__generated__/TrainingZone_user.graphql';
+import CourseCard from 'sharedComponents/Overview/CourseCard';
 
 const useStyles = createUseStyles((theme: Theme) => ({
   trainingZoneRoot: {
@@ -53,7 +54,13 @@ const useStyles = createUseStyles((theme: Theme) => ({
   },
   jumpHeader: {
     gridArea: 'jumpba'
-  }
+  },
+  courseHolder: {
+    display: 'grid',
+    gridGap: 26,
+    marginTop: theme.spacing(3),
+    gridTemplateColumns: 'repeat(auto-fit, 298px)'
+  },
 }));
 
 type Props = {
@@ -71,6 +78,8 @@ function TrainingZone({ user }: Props) {
       courses.push({ status: value.status, enrolledAt: value.enrolledAt });
     });
   }
+
+  const incomplete = user?.myCourses?.filter((course) => course.status === 'incomplete');
 
   return (
     <Page>
@@ -110,13 +119,34 @@ function TrainingZone({ user }: Props) {
           description={'Review your progress of all Courses in real time'}
           buttonProps={{ title: 'See Help Guides', onClick: () => {} }}
         />
-        <div className={classes.jumpHeader}>
-          <Heading text="Jump back in" size={'large'} />
-          <Heading
-            text={`${user?.firstName}, you have some unfinished courses,
-          jump back in to continue learning now.`}
-            size={'medium'}
-          />
+        {incomplete && incomplete.length > 0 && (
+          <div className={classes.jumpHeader}>
+            <Heading text="Jump back in" size={'large'} />
+            <Heading
+              text={`${user?.firstName}, you have some unfinished courses,
+            jump back in to continue learning now.`}
+              size={'medium'}
+            />
+          </div>
+        )}
+        <div className={classes.courseHolder}>
+          {incomplete && incomplete.map((course, index) => (
+              <CourseCard
+                key={index}
+                course={{
+                  title: course.course.name,
+                  id: course.course.ident,
+                  type: course.course.category?.name ?? 'Online Course',
+                  colour: course.course.category?.color ?? '',
+                  description: course.course.excerpt ?? '',
+                  url: course.course.bannerImageURL ?? ''
+                }}
+                onClick={() => {
+                  router.push(`/app/courses/${course.course.ident}`);
+                }}
+                progress={course.progress?.percent ?? 0}
+              />
+            ))}
         </div>
       </div>
     </Page>
@@ -128,8 +158,22 @@ export default createFragmentContainer(TrainingZone, {
     fragment TrainingZone_user on User {
       firstName
       myCourses {
+        course {
+          ident: id
+          name
+          excerpt
+          category {
+            color
+            name
+          }
+          type
+          bannerImageURL
+        }
         status
         enrolledAt
+        progress {
+          percent
+        }
       }
     }
   `
--- react/delegate/src/views/TrainingZone/__generated__/TrainingZone_user.graphql.ts
+++ react/delegate/src/views/TrainingZone/__generated__/TrainingZone_user.graphql.ts
@@ -4,11 +4,26 @@
 import { ReaderFragment } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type CourseStatus = "complete" | "failed" | "incomplete" | "%future added value";
+export type CourseType = "classroom" | "online" | "%future added value";
 export type TrainingZone_user = {
     readonly firstName: string;
     readonly myCourses: ReadonlyArray<{
+        readonly course: {
+            readonly ident: number;
+            readonly name: string;
+            readonly excerpt: string | null;
+            readonly category: {
+                readonly color: string;
+                readonly name: string;
+            } | null;
+            readonly type: CourseType;
+            readonly bannerImageURL: string | null;
+        };
         readonly status: CourseStatus;
         readonly enrolledAt: string;
+        readonly progress: {
+            readonly percent: number;
+        } | null;
     }> | null;
     readonly " $refType": "TrainingZone_user";
 };
@@ -20,46 +35,132 @@ export type TrainingZone_user$key = {
 
 
 
-const node: ReaderFragment = ({
-    "kind": "Fragment",
-    "name": "TrainingZone_user",
-    "type": "User",
-    "metadata": null,
-    "argumentDefinitions": [],
-    "selections": [
-        {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "firstName",
-            "args": null,
-            "storageKey": null
-        },
-        {
-            "kind": "LinkedField",
-            "alias": null,
-            "name": "myCourses",
-            "storageKey": null,
-            "args": null,
-            "concreteType": "MyCourse",
-            "plural": true,
-            "selections": [
-                {
-                    "kind": "ScalarField",
-                    "alias": null,
-                    "name": "status",
-                    "args": null,
-                    "storageKey": null
-                },
-                {
-                    "kind": "ScalarField",
-                    "alias": null,
-                    "name": "enrolledAt",
-                    "args": null,
-                    "storageKey": null
-                }
-            ]
-        }
-    ]
-} as any);
-(node as any).hash = '6c8ec02fd709f6a57443b0bc50c0bf9b';
+const node: ReaderFragment = (function () {
+    var v0 = ({
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "name",
+        "args": null,
+        "storageKey": null
+    } as any);
+    return {
+        "kind": "Fragment",
+        "name": "TrainingZone_user",
+        "type": "User",
+        "metadata": null,
+        "argumentDefinitions": [],
+        "selections": [
+            {
+                "kind": "ScalarField",
+                "alias": null,
+                "name": "firstName",
+                "args": null,
+                "storageKey": null
+            },
+            {
+                "kind": "LinkedField",
+                "alias": null,
+                "name": "myCourses",
+                "storageKey": null,
+                "args": null,
+                "concreteType": "MyCourse",
+                "plural": true,
+                "selections": [
+                    {
+                        "kind": "LinkedField",
+                        "alias": null,
+                        "name": "course",
+                        "storageKey": null,
+                        "args": null,
+                        "concreteType": "Course",
+                        "plural": false,
+                        "selections": [
+                            {
+                                "kind": "ScalarField",
+                                "alias": "ident",
+                                "name": "id",
+                                "args": null,
+                                "storageKey": null
+                            },
+                            (v0 /*: any*/),
+                            {
+                                "kind": "ScalarField",
+                                "alias": null,
+                                "name": "excerpt",
+                                "args": null,
+                                "storageKey": null
+                            },
+                            {
+                                "kind": "LinkedField",
+                                "alias": null,
+                                "name": "category",
+                                "storageKey": null,
+                                "args": null,
+                                "concreteType": "Category",
+                                "plural": false,
+                                "selections": [
+                                    {
+                                        "kind": "ScalarField",
+                                        "alias": null,
+                                        "name": "color",
+                                        "args": null,
+                                        "storageKey": null
+                                    },
+                                    (v0 /*: any*/)
+                                ]
+                            },
+                            {
+                                "kind": "ScalarField",
+                                "alias": null,
+                                "name": "type",
+                                "args": null,
+                                "storageKey": null
+                            },
+                            {
+                                "kind": "ScalarField",
+                                "alias": null,
+                                "name": "bannerImageURL",
+                                "args": null,
+                                "storageKey": null
+                            }
+                        ]
+                    },
+                    {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "status",
+                        "args": null,
+                        "storageKey": null
+                    },
+                    {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "name": "enrolledAt",
+                        "args": null,
+                        "storageKey": null
+                    },
+                    {
+                        "kind": "LinkedField",
+                        "alias": null,
+                        "name": "progress",
+                        "storageKey": null,
+                        "args": null,
+                        "concreteType": "Progress",
+                        "plural": false,
+                        "selections": [
+                            {
+                                "kind": "ScalarField",
+                                "alias": null,
+                                "name": "percent",
+                                "args": null,
+                                "storageKey": null
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    } as any;
+})();
+(node as any).hash = 'cfb6c5fc9cc21cde24212c8c8c585733';
 export default node;
